4ddfef60ede4abc475199c4cc5dc608aab0f292c
Didier Weckmann <didier.weckmann@ircad.fr>
2019-02-05 14:52:05 +0100

    fix(findpkg): make avresample optionnal

--- FindFFMPEG.cmake
+++ FindFFMPEG.cmake
@@ -1,3 +1,10 @@
+set(FFMPEG_FOUND FALSE)
+set(FFMPEG_libavcodec_FOUND FALSE)
+set(FFMPEG_libavformat_FOUND FALSE)
+set(FFMPEG_libavutil_FOUND FALSE)
+set(FFMPEG_libswscale_FOUND FALSE)
+set(FFMPEG_libavresample_FOUND FALSE)
+
 find_path(FFMPEG_INCLUDE_DIR NAMES "libavcodec/version.h" PATHS ${CONAN_INCLUDE_DIRS_FFMPEG})
 
 find_library(FFMPEG_libavcodec_LIBRARY NAMES avcodec PATHS ${CONAN_LIB_DIRS_FFMPEG})
@@ -6,16 +13,22 @@ find_library(FFMPEG_libavutil_LIBRARY NAMES avutil PATHS ${CONAN_LIB_DIRS_FFMPEG
 find_library(FFMPEG_libswscale_LIBRARY NAMES swscale PATHS ${CONAN_LIB_DIRS_FFMPEG})
 find_library(FFMPEG_libavresample_LIBRARY NAMES avresample PATHS ${CONAN_LIB_DIRS_FFMPEG})
 
-set(FFMPEG_FOUND TRUE)
-set(FFMPEG_libavcodec_FOUND TRUE)
-set(FFMPEG_libavformat_FOUND TRUE)
-set(FFMPEG_libavutil_FOUND TRUE)
-set(FFMPEG_libswscale_FOUND TRUE)
-set(FFMPEG_libavresample_FOUND TRUE)
+if(FFMPEG_INCLUDE_DIR AND FFMPEG_libavcodec_LIBRARY AND FFMPEG_libavformat_LIBRARY AND FFMPEG_libavutil_LIBRARY AND FFMPEG_libswscale_LIBRARY)
+    set(FFMPEG_FOUND TRUE)
+    set(FFMPEG_libavcodec_FOUND TRUE)
+    set(FFMPEG_libavformat_FOUND TRUE)
+    set(FFMPEG_libavutil_FOUND TRUE)
+    set(FFMPEG_libswscale_FOUND TRUE)
 
-set(FFMPEG_INCLUDE_DIRS ${FFMPEG_INCLUDE_DIR})
-set(FFMPEG_LIBRARY_DIRS ${CONAN_LIB_DIRS_FFMPEG})
-set(FFMPEG_LIBRARIES ${FFMPEG_libavcodec_LIBRARY} ${FFMPEG_libavformat_LIBRARY} ${FFMPEG_libavutil_LIBRARY} ${FFMPEG_libswscale_LIBRARY} ${FFMPEG_libavresample_LIBRARY})
+    set(FFMPEG_INCLUDE_DIRS ${FFMPEG_INCLUDE_DIR})
+    set(FFMPEG_LIBRARY_DIRS ${CONAN_LIB_DIRS_FFMPEG})
+    set(FFMPEG_LIBRARIES ${FFMPEG_libavcodec_LIBRARY} ${FFMPEG_libavformat_LIBRARY} ${FFMPEG_libavutil_LIBRARY} ${FFMPEG_libswscale_LIBRARY})
+
+    if(FFMPEG_libavresample_LIBRARY)
+        set(FFMPEG_libavresample_FOUND TRUE)
+        list(APPEND FFMPEG_LIBRARIES ${FFMPEG_libavresample_LIBRARY})
+    endif()
+endif()
 
 message(STATUS "(FFMPEG_FOUND : ${FFMPEG_FOUND} include: ${FFMPEG_INCLUDE_DIRS}, lib: ${FFMPEG_LIBRARIES})")
 
